97b746f4cef1da77d1725253fa01a30887580941
Art <art.mtsh@gmail.com>
2024-10-14 18:35:28 +0300

    CPU and memory reducing

--- modules/mod_levels_search.py
+++ modules/mod_levels_search.py
@@ -40,13 +40,13 @@ def upper_levels_check(c_high, i, w):
             if len(window) >= w and all(v <= check_list_max for v in window):
                 return check_list_max
 
-    wiggle_room = float(os.getenv('WIGGLE_ROOM_TWO', 0.08)) / 100
-    max_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_max >= v >= check_list_max - check_list_max * wiggle_room]
-    if len(max_indices) > 2:
-        for g in range(1, len(max_indices)):
-            window = check_list[max_indices[g - 1] + 1:max_indices[g]]
-            if len(window) >= w and all(v <= check_list_max for v in window):
-                return check_list_max
+    # wiggle_room = float(os.getenv('WIGGLE_ROOM_TWO', 0.08)) / 100
+    # max_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_max >= v >= check_list_max - check_list_max * wiggle_room]
+    # if len(max_indices) > 2:
+    #     for g in range(1, len(max_indices)):
+    #         window = check_list[max_indices[g - 1] + 1:max_indices[g]]
+    #         if len(window) >= w and all(v <= check_list_max for v in window):
+    #             return check_list_max
 
 
 def lower_levels_check(c_low, i, w):
@@ -68,13 +68,13 @@ def lower_levels_check(c_low, i, w):
             if len(window) >= w and all(v >= check_list_min for v in window):
                 return check_list_min
 
-    wiggle_room = float(os.getenv('WIGGLE_ROOM_TWO', 0.08)) / 100
-    min_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_min <= v <= check_list_min + check_list_min * wiggle_room]
-    if len(min_indices) > 2:
-        for g in range(1, len(min_indices)):
-            window = check_list[min_indices[g - 1] + 1:min_indices[g]]
-            if len(window) >= w and all(v >= check_list_min for v in window):
-                return check_list_min
+    # wiggle_room = float(os.getenv('WIGGLE_ROOM_TWO', 0.08)) / 100
+    # min_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_min <= v <= check_list_min + check_list_min * wiggle_room]
+    # if len(min_indices) > 2:
+    #     for g in range(1, len(min_indices)):
+    #         window = check_list[min_indices[g - 1] + 1:min_indices[g]]
+    #         if len(window) >= w and all(v >= check_list_min for v in window):
+    #             return check_list_min
 
 def levels_search(coins):
     for coin_data in coins:
