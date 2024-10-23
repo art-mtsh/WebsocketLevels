f23ea221efd661309c0c5cdd1e3254f40a9421d2
Art <art.mtsh@gmail.com>
2024-10-23 12:01:21 +0300

    reduced levels to 1m and 5m

--- modules/mod_levels_search.py
+++ modules/mod_levels_search.py
@@ -125,7 +125,7 @@ def levels_search(coins, complexity):
 
 
 async def levels_threads(coins_list):
-    complexity = 1 if len(coins_list) >= 40 else 2 if len(coins_list) >= 20 else 3
+    complexity = 1 if len(coins_list) >= 60 else 2 if len(coins_list) >= 30 else 3
     # personal_bot.send_message(personal_id, f"⚙️ Starting levels asyncio. Complexity: {complexity} ({len(coins_list)} coins)")
     coins_list = split_list(coins_list, 10)
 
