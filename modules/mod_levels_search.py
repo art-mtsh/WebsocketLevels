494efb4dcd21e96be5095c07fb7c71ddaa40026a
Art <art.mtsh@gmail.com>
2024-10-23 14:15:25 +0300

    improving CPU usage

--- modules/mod_levels_search.py
+++ modules/mod_levels_search.py
@@ -22,140 +22,139 @@ personal_bot = telebot.TeleBot(bot_token)
 personal_id = int(os.getenv('PERSONAL_ID'))
 
 
-def upper_levels_check(c_high, i, w, complexity):
+def upper_levels_check(c_high: list, c_close: float, x_atr_per, i: int, w: int):
+
     check_list = c_high[-i: -1]
     check_list_max = max(check_list)
-    max_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if v == check_list_max]
+    distance_validated = abs(check_list_max - c_close) / (c_close / 100) <= x_atr_per * 5
 
-    if len(max_indices) > 1:
-        for g in range(1, len(max_indices)):
-            window = check_list[max_indices[g - 1] + 1:max_indices[g]]
-            if len(window) >= w:  # and all(v <= check_list_max for v in window):
-                return check_list_max
+    if distance_validated:
+        max_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if v == check_list_max]
 
-    wiggle_room = float(os.getenv('WIGGLE_ROOM_ONE', 0.04)) / 100
-    max_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_max >= v >= check_list_max - check_list_max * wiggle_room]
-    if len(max_indices) > 1 and complexity > 1:
-        for g in range(1, len(max_indices)):
-            window = check_list[max_indices[g - 1] + 1:max_indices[g]]
-            if len(window) >= w and all(v <= check_list_max for v in window):
-                return check_list_max
+        if len(max_indices) > 1:
+            for g in range(1, len(max_indices)):
+                window = check_list[max_indices[g - 1] + 1:max_indices[g]]
+                if len(window) >= w:  # and all(v <= check_list_max for v in window):
+                    return check_list_max
 
-    wiggle_room = float(os.getenv('WIGGLE_ROOM_TWO', 0.08)) / 100
-    max_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_max >= v >= check_list_max - check_list_max * wiggle_room]
-    if len(max_indices) > 2 and complexity > 2:
-        for g in range(1, len(max_indices)):
-            window = check_list[max_indices[g - 1] + 1:max_indices[g]]
-            if len(window) >= w and all(v <= check_list_max for v in window):
-                return check_list_max
+        wiggle_room = float(os.getenv('WIGGLE_ROOM_ONE', 0.04)) / 100
+        max_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_max >= v >= check_list_max - check_list_max * wiggle_room]
+        if len(max_indices) > 1:
+            for g in range(1, len(max_indices)):
+                window = check_list[max_indices[g - 1] + 1:max_indices[g]]
+                if len(window) >= w and all(v <= check_list_max for v in window):
+                    return check_list_max
+
+        wiggle_room = float(os.getenv('WIGGLE_ROOM_TWO', 0.08)) / 100
+        max_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_max >= v >= check_list_max - check_list_max * wiggle_room]
+        if len(max_indices) > 2:
+            for g in range(1, len(max_indices)):
+                window = check_list[max_indices[g - 1] + 1:max_indices[g]]
+                if len(window) >= w and all(v <= check_list_max for v in window):
+                    return check_list_max
 
 
-def lower_levels_check(c_low, i, w, complexity):
+def lower_levels_check(c_low: list, c_close: float, x_atr_per, i: int, w: int):
+
     check_list = c_low[-i: -1]
     check_list_min = min(check_list)
-    min_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if v == check_list_min]
+    distance_validated = abs(check_list_min - c_close) / (c_close / 100) <= x_atr_per * 5
 
-    if len(min_indices) > 1:
-        for g in range(1, len(min_indices)):
-            window = check_list[min_indices[g - 1] + 1:min_indices[g]]
-            if len(window) >= w:  # and all(v >= check_list_min for v in window):
-                return check_list_min
+    if distance_validated:
+        min_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if v == check_list_min]
 
-    wiggle_room = float(os.getenv('WIGGLE_ROOM_ONE', 0.04)) / 100
-    min_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_min <= v <= check_list_min + check_list_min * wiggle_room]
-    if len(min_indices) > 1 and complexity > 1:
-        for g in range(1, len(min_indices)):
-            window = check_list[min_indices[g - 1] + 1:min_indices[g]]
-            if len(window) >= w and all(v >= check_list_min for v in window):
-                return check_list_min
+        if len(min_indices) > 1:
+            for g in range(1, len(min_indices)):
+                window = check_list[min_indices[g - 1] + 1:min_indices[g]]
+                if len(window) >= w:  # and all(v >= check_list_min for v in window):
+                    return check_list_min
 
-    wiggle_room = float(os.getenv('WIGGLE_ROOM_TWO', 0.08)) / 100
-    min_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_min <= v <= check_list_min + check_list_min * wiggle_room]
-    if len(min_indices) > 2 and complexity > 2:
-        for g in range(1, len(min_indices)):
-            window = check_list[min_indices[g - 1] + 1:min_indices[g]]
-            if len(window) >= w and all(v >= check_list_min for v in window):
-                return check_list_min
+        wiggle_room = float(os.getenv('WIGGLE_ROOM_ONE', 0.04)) / 100
+        min_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_min <= v <= check_list_min + check_list_min * wiggle_room]
+        if len(min_indices) > 1:
+            for g in range(1, len(min_indices)):
+                window = check_list[min_indices[g - 1] + 1:min_indices[g]]
+                if len(window) >= w and all(v >= check_list_min for v in window):
+                    return check_list_min
+
+        wiggle_room = float(os.getenv('WIGGLE_ROOM_TWO', 0.08)) / 100
+        min_indices = [b for b, v in enumerate(check_list[4:-w - 1]) if check_list_min <= v <= check_list_min + check_list_min * wiggle_room]
+        if len(min_indices) > 2:
+            for g in range(1, len(min_indices)):
+                window = check_list[min_indices[g - 1] + 1:min_indices[g]]
+                if len(window) >= w and all(v >= check_list_min for v in window):
+                    return check_list_min
 
 
-def levels_search(coins, complexity):
+def levels_search(coins):
     for coin_data in coins:
         symbol, ts_percent_futures, ts_percent_spot, x_atr_per = coin_data[0], coin_data[1], coin_data[2], coin_data[3]
 
         minute_spot_avg_volume = 0.0
         minute_futures_avg_volume = 0.0
         c_room = int(os.getenv('STARTING_ROOM'))  # стартова кімната з пошуку двох точок
-        frames = {'1m': 5, '5m': 1}
+        frames = {'1m': 5, '5m': 1, '15m': 1}
 
         for timeframe, window in frames.items():
             spot_klines = combined_klines(symbol, timeframe, 99, 'spot') if ts_percent_spot != 0 else None
             futu_klines = combined_klines(symbol, timeframe, 99, 'futures') if ts_percent_futures != 0 else None
 
-            if spot_klines:
-                s_high, s_low, s_close, avg_vol = spot_klines[2], spot_klines[3], spot_klines[4], spot_klines[5]
-                if timeframe == '1m':
-                    minute_spot_avg_volume = int(avg_vol)
-                for i in range(c_room, len(s_high)):
-                    upper = upper_levels_check(s_high, i, window, complexity)
-                    lower = lower_levels_check(s_low, i, window, complexity)
-                    if upper and futu_klines and (symbol, timeframe, 'spot', upper, max(futu_klines[2][-i: -1]), 'up') not in tracked_levels.keys():
-                        tracked_levels[(symbol, timeframe, 'spot', upper, max(futu_klines[2][-i: -1]), 'up')] = minute_spot_avg_volume, x_atr_per
-
-                    if lower and futu_klines and (symbol, timeframe, 'spot', lower, min(futu_klines[3][-i: -1]), 'dn') not in tracked_levels.keys():
-                        tracked_levels[(symbol, timeframe, 'spot', lower, min(futu_klines[3][-i: -1]), 'dn')] = minute_spot_avg_volume, x_atr_per
-
             if not futu_klines:
                 personal_bot.send_message(personal_id, f"⛔️ Main file. Error in {symbol} futures klines data!")
-
             else:
-                f_high, f_low, f_close, avg_vol = futu_klines[2], futu_klines[3], futu_klines[4], futu_klines[5]
-                if timeframe == '1m':
-                    minute_futures_avg_volume = int(avg_vol)
-                for i in range(c_room, len(f_high)):
-                    upper = upper_levels_check(f_high, i, window, complexity)
-                    lower = lower_levels_check(f_low, i, window, complexity)
-                    if upper and (symbol, timeframe, 'futures', upper, upper, 'up') not in tracked_levels.keys():
-                        tracked_levels[(symbol, timeframe, 'futures', upper, upper, 'up')] = minute_futures_avg_volume, x_atr_per
+                if spot_klines:
+                    s_high, s_low, s_close, avg_vol = spot_klines[2], spot_klines[3], spot_klines[4], spot_klines[5]
+                    if timeframe == '1m':
+                        minute_spot_avg_volume = int(avg_vol)
+                    for i in range(c_room, len(s_high)):
+                        upper = upper_levels_check(s_high, s_close[-1], x_atr_per, i, window)
+                        lower = lower_levels_check(s_low, s_close[-1], x_atr_per, i, window)
+                        if upper:
+                            if not any(key[3] == upper for key in tracked_levels):
+                                tracked_levels[(symbol, timeframe, "spot", upper, max(futu_klines[2][-i: -1]), "up")] = minute_spot_avg_volume, x_atr_per
+                        if lower:
+                            if not any(key[3] == lower for key in tracked_levels):
+                                tracked_levels[(symbol, timeframe, "spot", lower, min(futu_klines[3][-i: -1]), "dn")] = minute_spot_avg_volume, x_atr_per
 
-                    if lower and (symbol, timeframe, 'futures', lower, lower, 'dn') not in tracked_levels.keys():
-                        tracked_levels[(symbol, timeframe, 'futures', lower, lower, 'dn')] = minute_futures_avg_volume, x_atr_per
+                else:
+                    f_high, f_low, f_close, avg_vol = futu_klines[2], futu_klines[3], futu_klines[4], futu_klines[5]
+                    if timeframe == '1m':
+                        minute_futures_avg_volume = int(avg_vol)
+                    for i in range(c_room, len(f_high)):
+                        upper = upper_levels_check(f_high, f_close[-1], x_atr_per, i, window)
+                        lower = lower_levels_check(f_low, f_close[-1], x_atr_per, i, window)
+                        if upper:
+                            if not any(key[3] == upper for key in tracked_levels):
+                                tracked_levels[(symbol, timeframe, "futures", upper, upper, "up")] = minute_futures_avg_volume, x_atr_per
+                        if lower:
+                            if not any(key[3] == lower for key in tracked_levels):
+                                tracked_levels[(symbol, timeframe, "futures", lower, lower, "dn")] = minute_futures_avg_volume, x_atr_per
 
-        # time.sleep(5)  # every 6 seconds 10 threads do 3 requests with 3 weights, which is 900- weights per minute
-        time.sleep(4)  # every 6 seconds 10 threads do 2 requests with 3 weights, which is 900- weights per minute
+        time.sleep(5)  # every 6 seconds 10 threads do 3 requests with 3 weights, which is 900- weights per minute
 
 
-async def levels_threads(coins_list):
-    complexity = 1 if len(coins_list) >= 30 else 2 if len(coins_list) >= 15 else 3
-    # personal_bot.send_message(personal_id, f"⚙️ Starting levels asyncio. Complexity: {complexity} ({len(coins_list)} coins)")
-    coins_list = split_list(coins_list, 10)
+async def levels_threads(coins_top_list):
+    coins_list = split_list(coins_top_list, 10)
 
     the_threads = []
     for coins in coins_list:
-        thread = threading.Thread(target=levels_search, args=(coins, complexity,))
+        thread = threading.Thread(target=levels_search, args=(coins,))
         thread.start()
         the_threads.append(thread)
     for thread in the_threads:
         await asyncio.to_thread(thread.join)
-    msg = f'⚙️ Found {len(tracked_levels)} levels'
-
-    # personal_bot.send_message(personal_id, msg)
     await asyncio.sleep(60)
 
     while not global_stop.is_set():
         m, s = datetime.now().strftime('%M'), datetime.now().strftime('%S')
         if int(m) % 5 == 0 and int(s) == 0:
-            # personal_bot.send_message(personal_id, f"⚙️ Refreshing levels. Complexity: {complexity} ({len(coins_list)} coins)")
-            copy_levels = tracked_levels.copy()
-
             the_threads = []
             for coins in coins_list:
-                thread = threading.Thread(target=levels_search, args=(coins, complexity,))
+                thread = threading.Thread(target=levels_search, args=(coins,))
                 thread.start()
                 the_threads.append(thread)
             for thread in the_threads:
                 await asyncio.to_thread(thread.join)
-            msg = f'⚙️ Added {len(tracked_levels) - len(copy_levels)} levels, now tracked levels count: {len(tracked_levels)}'
-            # personal_bot.send_message(personal_id, msg)
         await asyncio.sleep(0.1)
 
     personal_bot.send_message(personal_id, f"⚙️ Levels asyncio done its work.")
--- modules/mod_market_depth_listener.py
+++ modules/mod_market_depth_listener.py
@@ -21,73 +21,67 @@ personal_id = int(os.getenv('PERSONAL_ID'))
 tracked_levels_lock = asyncio.Lock()
 
 
-def process_depth(m_type, coin, bids: dict, asks: dict, dropped_levels: set, tracked) -> tuple or None:
+def process_depth(coin, market_type, bids: dict, asks: dict, level, side, avg_vol, atr) -> tuple or None:
     best_bid: float = list(bids.keys())[-1]
     best_ask: float = list(bids.keys())[0]
 
-    for key, value in tracked.items():
-        symbol, timeframe, market_type, origin_level, futures_according_level, side, avg_vol, atr = key[0], key[1], key[2], key[3], key[4], key[5], value[0], value[1]
-        level = origin_level if market_type == 'spot' else futures_according_level
-        # current_distance = abs(level - (best_ask + best_bid) / 2) / (level / 100)
-        # current_distance = round(current_distance, 2)
-        h, m = datetime.now().strftime('%H'), datetime.now().strftime('%M')
+    h, m = datetime.now().strftime('%H'), datetime.now().strftime('%M')
 
-        if symbol == coin and market_type == m_type and key not in dropped_levels:
-            avg_vol_mpl = float(os.getenv('AVG_VOL_MPL'))
-            sec_vol_mpl = float(os.getenv('SEC_VOL_MPL'))
-            atr_dis_mpl = float(os.getenv('ATR_DIS_MPL'))
-            best_price_dist = float(os.getenv('BEST_PRICE_DIST'))
+    avg_vol_mpl = float(os.getenv('AVG_VOL_MPL'))
+    sec_vol_mpl = float(os.getenv('SEC_VOL_MPL'))
+    atr_dis_mpl = float(os.getenv('ATR_DIS_MPL'))
+    best_price_dist = float(os.getenv('BEST_PRICE_DIST'))
 
-            if side == 'up':
-                if best_ask > level:
-                    return key
-                else:
-                    max_volume_price, max_volume_volume = sorted(asks.items(), key=lambda x: x[1])[-1]
-                    second_volume_price, second_volume_volume = sorted(asks.items(), key=lambda x: x[1])[-2]
-                    distance_to_max = round(abs(best_ask - max_volume_price) / (max_volume_price / 100), 2)
+    if side == 'up':
+        if best_ask > level:
+            return True
+        else:
+            max_volume_price, max_volume_volume = sorted(asks.items(), key=lambda x: x[1])[-1]
+            second_volume_price, second_volume_volume = sorted(asks.items(), key=lambda x: x[1])[-2]
+            distance_to_max = round(abs(best_ask - max_volume_price) / (max_volume_price / 100), 2)
 
-                    max_vol_verified = max_volume_volume >= avg_vol * avg_vol_mpl
-                    sec_vol_verified = max_volume_volume >= second_volume_volume * sec_vol_mpl
-                    max_vol_close_to_level = level >= max_volume_price >= level - level * ((atr * atr_dis_mpl) / 100)
-                    price_dist_to_max = distance_to_max <= best_price_dist
+            max_vol_verified = max_volume_volume >= avg_vol * avg_vol_mpl
+            sec_vol_verified = max_volume_volume >= second_volume_volume * sec_vol_mpl
+            max_vol_close_to_level = level >= max_volume_price >= level - level * ((atr * atr_dis_mpl) / 100)
+            price_dist_to_max = distance_to_max <= best_price_dist
 
-                    msg = (f'{symbol} ({market_type})\n'
-                           f'{"☑️" if max_vol_verified else "◻️"} max_vol_verified: {int(max_volume_volume / 1000)}k ({round(max_volume_volume / avg_vol, 2)} x avg.vol)\n'
-                           f'{"☑️" if max_vol_close_to_level else "◻️"} max_vol_close_to_level: {level}\n'
-                           f'{"☑️" if price_dist_to_max else "◻️"} max_dist_ver: {distance_to_max}% to max vol\n')
+            msg = (f'{coin} ({market_type})\n'
+                   f'{"☑️" if max_vol_verified else "◻️"} max_vol_verified: {int(max_volume_volume / 1000)}k ({round(max_volume_volume / avg_vol, 2)} x avg.vol)\n'
+                   f'{"☑️" if max_vol_close_to_level else "◻️"} max_vol_close_to_level: {level}\n'
+                   f'{"☑️" if price_dist_to_max else "◻️"} max_dist_ver: {distance_to_max}% to max vol\n')
 
-                    if max_vol_verified and max_vol_close_to_level and price_dist_to_max and sec_vol_verified:
-                        if (symbol, h, level, 111) not in sent_messages:
-                            personal_bot.send_message(personal_id, '✅ TRADE\n' + msg)
-                            sent_messages.append((symbol, h, level, 111))
-                            print('✅ TRADE\n' + msg)
+            if max_vol_verified and max_vol_close_to_level and price_dist_to_max and sec_vol_verified:
+                if (coin, h, level, 111) not in sent_messages:
+                    personal_bot.send_message(personal_id, '✅ TRADE\n' + msg)
+                    sent_messages.append((coin, h, level, 111))
+                    print('✅ TRADE\n' + msg)
 
-            if side == 'dn':
-                if best_bid < level:
-                    return key
-                else:
-                    max_volume_price, max_volume_volume = sorted(bids.items(), key=lambda x: x[1])[-1]
-                    second_volume_price, second_volume_volume = sorted(bids.items(), key=lambda x: x[1])[-2]
-                    distance_to_max = round(abs(best_bid - max_volume_price) / (max_volume_price / 100), 2)
+    if side == 'dn':
+        if best_bid < level:
+            return True
+        else:
+            max_volume_price, max_volume_volume = sorted(bids.items(), key=lambda x: x[1])[-1]
+            second_volume_price, second_volume_volume = sorted(bids.items(), key=lambda x: x[1])[-2]
+            distance_to_max = round(abs(best_bid - max_volume_price) / (max_volume_price / 100), 2)
 
-                    max_vol_verified = max_volume_volume >= avg_vol * avg_vol_mpl
-                    sec_vol_verified = max_volume_volume >= second_volume_volume * sec_vol_mpl
-                    max_vol_close_to_level = level <= max_volume_price <= level + level * ((atr * atr_dis_mpl) / 100)
-                    price_dist_to_max = distance_to_max <= best_price_dist
+            max_vol_verified = max_volume_volume >= avg_vol * avg_vol_mpl
+            sec_vol_verified = max_volume_volume >= second_volume_volume * sec_vol_mpl
+            max_vol_close_to_level = level <= max_volume_price <= level + level * ((atr * atr_dis_mpl) / 100)
+            price_dist_to_max = distance_to_max <= best_price_dist
 
-                    msg = (f'{symbol} ({market_type})\n'
-                           f'{"☑️" if max_vol_verified else "◻️"} max_vol_verified: {int(max_volume_volume / 1000)}k ({round(max_volume_volume / avg_vol, 2)} x avg.vol)\n'
-                           f'{"☑️" if max_vol_close_to_level else "◻️"} max_vol_close_to_level: {level}\n'
-                           f'{"☑️" if price_dist_to_max else "◻️"} max_dist_ver: {distance_to_max}% to max vol\n')
+            msg = (f'{coin} ({market_type})\n'
+                   f'{"☑️" if max_vol_verified else "◻️"} max_vol_verified: {int(max_volume_volume / 1000)}k ({round(max_volume_volume / avg_vol, 2)} x avg.vol)\n'
+                   f'{"☑️" if max_vol_close_to_level else "◻️"} max_vol_close_to_level: {level}\n'
+                   f'{"☑️" if price_dist_to_max else "◻️"} max_dist_ver: {distance_to_max}% to max vol\n')
 
-                    if max_vol_verified and max_vol_close_to_level and price_dist_to_max and sec_vol_verified:
-                        if (symbol, h, level, 444) not in sent_messages:
-                            personal_bot.send_message(personal_id, '✅ TRADE\n' + msg)
-                            sent_messages.append((symbol, h, level, 444))
-                            print('✅ TRADE\n' + msg)
+            if max_vol_verified and max_vol_close_to_level and price_dist_to_max and sec_vol_verified:
+                if (coin, h, level, 444) not in sent_messages:
+                    personal_bot.send_message(personal_id, '✅ TRADE\n' + msg)
+                    sent_messages.append((coin, h, level, 444))
+                    print('✅ TRADE\n' + msg)
 
-            if side not in ['up', 'dn']:
-                print('Wrong side! : ', side)
+    if side not in ['up', 'dn']:
+        print('Wrong side! : ', side)
 
 
 async def connect_and_listen(stream_url):
@@ -113,27 +107,28 @@ async def connect_and_listen(stream_url):
                             break
                         response = json.loads(message)
 
-                        levels_symbols = set([s[0] for s in tr_levels.keys()])
+                        # levels_symbols = set([s[0] for s in tr_levels.keys()])
                         coin = response['stream'].split('@')[0].upper()
                         data = response['data']
 
-                        # spot
-                        if 'bids' in data.keys() and coin in levels_symbols:
-                            m_type = 'spot'
-                            bids, asks = data['bids'][::-1], data['asks']
-                            bids, asks = {float(v[0]): float(v[1]) for v in bids}, {float(v[0]): float(v[1]) for v in asks}
-                            if de := process_depth(m_type, coin, bids, asks, dropped_levels, tr_levels):
-                                dropped_levels.add(de)
-                                # personal_bot.send_message(personal_id, f'Level added to dropped levels: {de}')
+                        for key, value in tr_levels.items():
+                            symbol, timeframe, market_type, origin_level, futures_according_level, side, avg_vol, atr = key[0], key[1], key[2], key[3], key[4], key[5], value[0], value[1]
 
-                        # futures
-                        elif 'b' in data.keys() and coin in levels_symbols:
-                            m_type = 'futures'
-                            bids, asks = data['b'][::-1], data['a']
-                            bids, asks = {float(v[0]): float(v[1]) for v in bids}, {float(v[0]): float(v[1]) for v in asks}
-                            if de := process_depth(m_type, coin, bids, asks, dropped_levels, tr_levels):
-                                dropped_levels.add(de)
-                                # personal_bot.send_message(personal_id, f'Level added to dropped levels: {de}')
+                            # spot
+                            if 'bids' in data.keys() and market_type == 'spot' and coin == symbol and key not in dropped_levels:
+                                bids, asks = data['bids'][::-1], data['asks']
+                                bids, asks = {float(v[0]): float(v[1]) for v in bids}, {float(v[0]): float(v[1]) for v in asks}
+                                if process_depth(coin, market_type, bids, asks, origin_level, side, avg_vol, atr):
+                                    dropped_levels.add(key)
+                                    personal_bot.send_message(personal_id, f'Level added to dropped levels: {key}')
+
+                            # futures
+                            elif 'b' in data.keys() and market_type == 'futures' and coin == symbol and key not in dropped_levels:
+                                bids, asks = data['b'][::-1], data['a']
+                                bids, asks = {float(v[0]): float(v[1]) for v in bids}, {float(v[0]): float(v[1]) for v in asks}
+                                if process_depth(coin, market_type, bids, asks, origin_level, side, avg_vol, atr):
+                                    dropped_levels.add(key)
+                                    personal_bot.send_message(personal_id, f'Level added to dropped levels: {key}')
 
                     except websockets.exceptions.ConnectionClosed:
                         personal_bot.send_message(personal_id, "Connection closed.")
